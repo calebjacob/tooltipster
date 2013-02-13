e0a8620563f349c69f0c66ea7c409e16b76614a0
Caleb Jacob <hello@calebjacob.com>
2013-02-12 20:37:35 -0700

    Tooltips will now be removed if its origin is removed

--- js/jquery.tooltipster.js
+++ js/jquery.tooltipster.js
@@ -304,8 +304,14 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 						// check to see if our tooltip content changes or its origin is removed while the tooltip is alive
 						var currentTooltipContent = content;
 						var contentUpdateChecker = setInterval(function() {		
-							var newTooltipContent = $this.data('tooltipsterContent');												
-							if ((currentTooltipContent !== newTooltipContent) && (newTooltipContent !== '')) {
+							var newTooltipContent = $this.data('tooltipsterContent');
+							
+							if ($('body').find($this).length == 0) {
+								tooltipster.addClass('tooltipster-dying');
+								object.hideTooltip();
+							}
+																			
+							else if ((currentTooltipContent !== newTooltipContent) && (newTooltipContent !== '')) {
 								currentTooltipContent = newTooltipContent;
 								
 								tooltipster.find('.tooltipster-content').html(newTooltipContent);
@@ -344,13 +350,8 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 								
 								object.positionTooltip();
 							}
-
-							// if the origin is removed, remove the tooltip
-							if ($('body').find($this).length == 0) {
-								object.hideTooltip();
-							}
 							
-							// if the tooltip is closed, stop this interval
+							// if the tooltip is closed or origin is removed, stop this interval
 							if (($('body').find(tooltipster).length == 0) || ($('body').find($this).length == 0)) {
 								clearInterval(contentUpdateChecker);
 							}
@@ -405,7 +406,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 		},
 		
 		hideTooltip: function(options) {
-			
+						
 			var $this = $(this.element);
 			var object = this;
 			
@@ -415,6 +416,11 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 			}
 			
 			var tooltipster = $this.data('tooltipster');
+			
+			// if the origin has been removed, find all tooltips assigned to death
+			if (tooltipster == undefined) {
+				tooltipster = $('.tooltipster-dying');
+			}
 			
 			// clear any possible queues handling delays and such
 			$this.clearQueue();
