10a825447e79f6ae7837db89af90d4b97d5c4d60
Paul Annekov <paul.annekov@gmail.com>
2013-08-31 13:20:12 +0300

    Prevent <script> tags execution on show

--- js/jquery.tooltipster.js
+++ js/jquery.tooltipster.js
@@ -283,7 +283,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 							$('body').css('overflow-x', 'hidden');
 							
 							// get the content for the tooltip
-							var content = $this.data('tooltipsterContent');
+							var content = object.getContent($this);
 							
 							// get some other settings related to building the tooltip
 							var theme = object.options.theme;
@@ -324,7 +324,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 							// check to see if our tooltip content changes or its origin is removed while the tooltip is alive
 							var currentTooltipContent = content;
 							var contentUpdateChecker = setInterval(function() {		
-								var newTooltipContent = $this.data('tooltipsterContent');
+								var newTooltipContent = object.getContent($this);
 								
 								// if this tooltip's origin is removed, remove the tooltip
 								if ($('body').find($this).length == 0) {
@@ -335,7 +335,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 								// if the content changed for the tooltip, update it											
 								else if ((currentTooltipContent !== newTooltipContent) && (newTooltipContent !== '')) {
 									currentTooltipContent = newTooltipContent;
-									
+
 									// set the new content in the tooltip
 									tooltipster.find('.tooltipster-content').html(newTooltipContent);
 									
@@ -483,9 +483,9 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 				}
 			}
 		},
-		
+
 		positionTooltip: function(options) {
-					
+
 			var $this = $(this.element);
 			var object = this;
 									
@@ -834,7 +834,14 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 					object.options.position = resetPosition;
 				}
 			}
-		}
+		},
+    getContent: function(element) {
+      var content = element.data('tooltipsterContent');
+      // will remove <script> tags to prevent XSS (execution of JS for dynamic tooltips)
+      content = $($.parseHTML('<div>' + content + '</div>')).html();
+
+      return content;
+    }
 	};
 		
 	$.fn[pluginName] = function (options) {
