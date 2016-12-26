74226b87b74bfcf702786af3c5b1c11ee461b748
cybermerlin <cybermerlin@ya.ru>
2016-12-26 21:12:50 +0300

    #639
    #639

--- src/js/tooltipster.js
+++ src/js/tooltipster.js
@@ -2921,9 +2921,6 @@ $.fn.tooltipster = function() {
 						return false;
 					}
 				}
-				else {
-					throw new Error('You called Tooltipster\'s "'+ args[0] +'" method on an uninitialized element');
-				}
 			});
 			
 			return (v !== '#*$~&') ? v : this;
