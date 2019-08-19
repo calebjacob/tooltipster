f818f589de54165209756aa3319f49f841d708a2
louisameline <evangun2001@yahoo.fr>
2019-08-19 15:35:48 +0200

    version bump

--- dist/js/plugins/tooltipster/SVG/tooltipster-SVG.js
+++ dist/js/plugins/tooltipster/SVG/tooltipster-SVG.js
@@ -1,34 +1,32 @@
 (function (root, factory) {
-  if (root === undefined && window !== undefined) root = window;
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module unless amdModuleId is set
     define(["tooltipster"], function (a0) {
       return (factory(a0));
     });
-  } else if (typeof module === 'object' && module.exports) {
+  } else if (typeof exports === 'object') {
     // Node. Does not work with strict CommonJS, but
     // only CommonJS-like environments that support module.exports,
     // like Node.
     module.exports = factory(require("tooltipster"));
   } else {
-    factory(root["jQuery"]);
+    factory(jQuery);
   }
 }(this, function ($) {
 
 (function (root, factory) {
-  if (root === undefined && window !== undefined) root = window;
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module unless amdModuleId is set
     define(["jquery"], function (a0) {
       return (factory(a0));
     });
-  } else if (typeof module === 'object' && module.exports) {
+  } else if (typeof exports === 'object') {
     // Node. Does not work with strict CommonJS, but
     // only CommonJS-like environments that support module.exports,
     // like Node.
     module.exports = factory(require("jquery"));
   } else {
-    factory(root["jQuery"]);
+    factory(jQuery);
   }
 }(this, function ($) {
 
--- dist/js/tooltipster.main.js
+++ dist/js/tooltipster.main.js
@@ -5,19 +5,18 @@
  * MIT license
  */
 (function (root, factory) {
-  if (root === undefined && window !== undefined) root = window;
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module unless amdModuleId is set
     define(["jquery"], function (a0) {
       return (factory(a0));
     });
-  } else if (typeof module === 'object' && module.exports) {
+  } else if (typeof exports === 'object') {
     // Node. Does not work with strict CommonJS, but
     // only CommonJS-like environments that support module.exports,
     // like Node.
     module.exports = factory(require("jquery"));
   } else {
-    factory(root["jQuery"]);
+    factory(jQuery);
   }
 }(this, function ($) {
 
@@ -90,7 +89,7 @@ var defaults = {
 		hasTransitions: transitionSupport(),
 		IE: false,
 		// don't set manually, it will be updated by a build task after the manifest
-		semVer: '4.2.6',
+		semVer: '4.2.7',
 		window: win
 	},
 	core = function() {
